bd7c7d5c95a1ef4cd4530071d2ff66b47adbeeeb
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-08-02 04:33:18 +0530

    Updates

---
+++ precache-manifest.8ec03c5b59499838c0a7d1a9c1039ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fdeee045fe6b81a6426c5ee5fb0c0b",
+    "url": "/TaskEcart/index.html"
+  },
+  {
+    "revision": "50104cbb741b94501781",
+    "url": "/TaskEcart/static/css/main.14a07126.chunk.css"
+  },
+  {
+    "revision": "d67bf0e695a4ba073e80",
+    "url": "/TaskEcart/static/js/2.fc0065a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TaskEcart/static/js/2.fc0065a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50104cbb741b94501781",
+    "url": "/TaskEcart/static/js/main.4c19ac83.chunk.js"
+  },
+  {
+    "revision": "affa7609b48ae96cc05a",
+    "url": "/TaskEcart/static/js/runtime-main.0eee3c29.js"
+  },
+  {
+    "revision": "d8f89426ec905c7c482c0501e13bed17",
+    "url": "/TaskEcart/static/media/data.d8f89426.csv"
+  }
+]);
